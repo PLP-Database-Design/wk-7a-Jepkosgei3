2da58c6e5734740b90274091676486e6e2fff1de
Jepkosgei3 <95687538+Jepkosgei3@users.noreply.github.com>
2025-04-27 16:47:02 +0300

    Created data for 1NF and 2NF in answers.sql

---
+++ answers.sql
@@ -0,0 +1,40 @@
+
+CREATE TABLE OrderDetails_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50)
+);
+
+
+INSERT INTO OrderDetails_1NF (OrderID, CustomerName, Product)
+VALUES 
+    (101, 'John Doe', 'Laptop'),
+    (101, 'John Doe', 'Mouse'),
+    (102, 'Jane Smith', 'Tablet'),
+    (102, 'Jane Smith', 'Keyboard'),
+    (102, 'Jane Smith', 'Mouse'),
+    (103, 'Emily Clark', 'Phone');
+
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
